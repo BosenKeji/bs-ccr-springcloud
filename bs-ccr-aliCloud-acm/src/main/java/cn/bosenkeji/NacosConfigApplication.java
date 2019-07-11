deb386ea3be6b4e112586a3fca3b170922b0cb02
yuxuewen <8586826@qq.com>
2019-07-11 15:23:22 +0800

    add nacos

---
+++ bs-ccr-aliCloud-acm/src/main/java/cn/bosenkeji/NacosConfigApplication.java
@@ -0,0 +1,19 @@
+package cn.bosenkeji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName AcmApp
+ * @Description TODO
+ * @Author Yu XueWen
+ * @Email 8586826@qq.com
+ * @Versio V1.0
+ **/
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
--- bs-ccr-provider-coin/src/main/java/cn/bosenkeji/CoinApp.java
+++ bs-ccr-provider-coin/src/main/java/cn/bosenkeji/CoinApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ClassName App
@@ -16,7 +15,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 **/
 @SpringBootApplication
 @MapperScan("cn.bosenkeji.mapper")
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 public class CoinApp {
