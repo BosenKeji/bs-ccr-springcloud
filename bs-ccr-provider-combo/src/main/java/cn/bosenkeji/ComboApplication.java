228df0607fe3d1009823b7c3ac412e5c5a86fd2f
xivinChen <12250402127@qq.com>
2019-07-15 20:34:32 +0800

    陈醒文——完成了产品和套餐的crud

---
+++ bs-ccr-provider-combo/src/main/java/cn/bosenkeji/ComboApplication.java
@@ -0,0 +1,19 @@
+package cn.bosenkeji;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@MapperScan("cn.bosenkeji.mapper")
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class ComboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComboApplication.class, args);
+    }
+
+}
---
+++ bs-ccr-provider-product/src/main/java/cn/bosenkeji/service/IProductService.java
@@ -0,0 +1,24 @@
+package cn.bosenkeji.service;
+
+import cn.bosenkeji.vo.Product;
+
+import java.util.List;
+
+/**
+ * @author xivin
+ * @create 2019-07-11 11:37
+ */
+public interface IProductService {
+
+    List<Product> list();
+    Product get(int id);
+
+    boolean add(Product product);
+
+    boolean delete(int id);
+
+    boolean update(Product product);
+
+    boolean updateStatus(int id, int status);
+
+}
