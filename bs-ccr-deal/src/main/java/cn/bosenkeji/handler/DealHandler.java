a99345cb02c7d49e541ce6e4d57b4ea5a65d1436
hjh <hjhopq@163.com>
2019-07-27 20:06:52 +0800

    deal模块确定基本买卖的逻辑，接通mq和redis

--- bs-ccr-deal/src/main/java/cn/bosenkeji/handler/DealHandler.java
+++ bs-ccr-deal/src/main/java/cn/bosenkeji/handler/DealHandler.java
@@ -1,11 +1,41 @@
 package cn.bosenkeji.handler;
 
+import cn.bosenkeji.messaging.MySource;
 
+import cn.bosenkeji.service.IStrategyService;
+import cn.bosenkeji.vo.strategy.Strategy;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+
+@RestController
 public class DealHandler {
+
+    @Autowired
+    private MySource source;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private IStrategyService strategyService;
+
+
+    @RequestMapping("/strategy")
+    public PageInfo testConsumer() {
+        PageInfo<Strategy> strategyPageInfo = strategyService.listByPage(1, 10);
+        return strategyPageInfo;
+    }
 
 
     /**
-     * 计算实时收益比
+     * 计算实时收益比  买价*持仓数量/持仓费用  精确小数点后4位
      * @param number 持仓数量
      * @param cost 持仓费用
      * @param realTimePrice 实时买价
@@ -15,34 +45,56 @@ public class DealHandler {
         return realTimePrice*number/cost;
     }
 
+    /**
+     * 计算拟买入均价
+     * @param deep 实时深度
+     * @param quantity 某交易量
+     * @return 拟买入均价
+     */
+    private double countAveragePrice(Map<Integer,Double> deep,double quantity) {
+        double priceSum = 0;
+        double deepSum = 0;
+        for (Map.Entry<Integer, Double> entry : deep.entrySet()) {
+            Integer key = entry.getKey();
+            Double value = entry.getValue();
+            if ( quantity >= value) {
+                priceSum += key*value;
+                quantity -= value;
+                deepSum += value;
+            } else {
+                priceSum += key*quantity;
+                deepSum += quantity;
+            }
+        }
+        return priceSum/deepSum;
+    }
+
 
     /**
-     * 确定买的逻辑
+     * 是否买的逻辑
+     * @param realTimePrice 实时价格 mq获取
+     * @param orderNumber 已做单数 redis获取
+     * @param maxOrderNumber 最大交易单数 redis获取
+     * @param averagePosition 持仓均价 调用上面的公式
+     * @param buildPositionInterval 建仓间隔 redis获取
+     * @param averagePrice 拟买入均价 调用上面公式
+     * @return 是否买
      */
 
     public boolean isBuy(double realTimePrice, int orderNumber, int maxOrderNumber,
                          double averagePosition, double buildPositionInterval,double averagePrice) {
-        //是否需要判断？ 达到最大交易单数？ 用户已暂停或停止？ Redis获取
+        //是否需要判断？ 达到最大交易单数？
         if ( orderNumber == maxOrderNumber ) {
             return false;
         }
-
         //是否为第一单？ 第一单直接购买
         if ( orderNumber == 0 ) {
             return true;
         }
-
-        //TODO 计算持仓均价 参数传入 Redis取
-        // 读取建仓间隔 Redis取
-        // 读取交易速率 用于计算最大交易次数，最大交易次数从Redis里面取
-        // 读取已做单数 Redis取
-
         //现价是否小于等于整体持仓均价-建仓间隔*N？
         if ( realTimePrice > (averagePosition-buildPositionInterval) ) {
             return false;
         }
-        //TODO 读取下一单买入数量 Redis获取 暂时MOCK @hjh 2019.7.26
-        double nextOrderQuantity = 0;
 
         //计算拟买入均价 参数传入
 
@@ -69,45 +121,49 @@ public class DealHandler {
         return false;
     }
 
+    /**
+     *  确定买的逻辑
+     * @param positionPrice 持仓费用
+     * @param stopProfitType 止盈方式
+     * @param stopProfitPrice 止盈金额
+     * @param stopProfitRatio 止盈比例
+     * @param realTimeEarningRatio 实时收益比
+     * @param triggerRatio 触发比例
+     * @param callBackRatio 回调比例
+     * @return 是否卖
+     */
 
-    public boolean isSell(double positionPrice,int stopProfitType) {
-
-        //TODO 读取持仓费用 参数传入 Redis获取
-
+    public boolean isSell(double positionPrice, int stopProfitType, double stopProfitPrice
+            ,double stopProfitRatio ,double realTimeEarningRatio,double triggerRatio, double callBackRatio) {
         //读取止盈金额和止盈比例，两种止盈方式，达到一种即可 参数传入
-
         //判断是否启用追踪止盈 if-else  1为追踪止盈，2为固定止盈
         boolean isStopProfitTrace = (stopProfitType==1) ? true : false;
 
+        //持仓费用*（收益比-1）≥止盈金额？ 确定卖出
+        if ( (positionPrice * (realTimeEarningRatio-1)) >= stopProfitPrice ) {
+            return true;
+        }
         if (isStopProfitTrace) {
             //追踪止盈逻辑
-            //持仓费用*（收益比-1）≥止盈金额？ 确定卖出
 
-            //收益比≥1+触发比例？
-
-            //记录实时收益比的最高数值
-
-            //实时收益比≤最高实时收益比-回降比例？ 确定卖出
-
+            //收益比≥1+触发比例？ 追踪止盈
+            if (realTimeEarningRatio >= (1 + triggerRatio)) {
+                //TODO 记录实时收益比的最高数值  抽想一个方法用于更改从redis获取的JSON字符串
+                double maxEarningRation = 0;
+                //实时收益比≤最高实时收益比-回降比例？ 确定卖出
+                if (realTimeEarningRatio <= (maxEarningRation-callBackRatio)) {
+                    return true;
+                }
+            }
         } else {
-            //固定止盈
-            //持仓费用*（收益比-1）≥止盈金额？ //确定卖出
-
             //收益比≥1+止盈比例？ //确定卖出
+            if (realTimeEarningRatio > (1 + stopProfitRatio)) {
+                return true;
+            }
         }
-
-
         return false;
     }
 
 
-    /**
-     * 计算拟买入均价
-     * @param realTimePrice 实时价格
-     * @return 拟买入均价
-     */
-    private double countAveragePrice(double realTimePrice) {
 
-        return 0;
-    }
 }
--- bs-ccr-redis/src/main/java/cn/bosenkeji/RedisApp.java
+++ bs-ccr-redis/src/main/java/cn/bosenkeji/RedisApp.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 /**
  * Hello world!
@@ -13,8 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableCaching
-@EnableDiscoveryClient
-@EnableCircuitBreaker
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
 public class RedisApp
 {
     public static void main( String[] args )
