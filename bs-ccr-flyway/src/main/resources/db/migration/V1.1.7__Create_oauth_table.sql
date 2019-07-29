cceeb6c4c6bc2369e78565e62ee85e30fba02a8d
yuxuewen <8586826@qq.com>
2019-07-30 03:05:19 +0800

    ready to aliCloud

--- bs-ccr-flyway/src/main/resources/db/migration/V1.1.7__Create_oauth_table.sql
+++ bs-ccr-flyway/src/main/resources/db/migration/V1.1.7__Create_oauth_table.sql
@@ -1,6 +1,7 @@
 drop table if exists oauth_client_details;
 create table oauth_client_details (
-  client_id VARCHAR(255) PRIMARY KEY,
+  id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  client_id VARCHAR(255) ,
   resource_ids VARCHAR(255),
   client_secret VARCHAR(255),
   scope VARCHAR(255),
@@ -15,18 +16,20 @@ create table oauth_client_details (
  
 drop table if exists oauth_client_token;
 create table oauth_client_token (
+  id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   token_id VARCHAR(255),
   token LONG VARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
+  authentication_id VARCHAR(255),
   user_name VARCHAR(255),
   client_id VARCHAR(255)
 );
  
 drop table if exists oauth_access_token;
 create table oauth_access_token (
+  id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   token_id VARCHAR(255),
   token LONG VARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
+  authentication_id VARCHAR(255),
   user_name VARCHAR(255),
   client_id VARCHAR(255),
   authentication LONG VARBINARY,
@@ -35,6 +38,7 @@ create table oauth_access_token (
  
 drop table if exists oauth_refresh_token;
 create table oauth_refresh_token (
+  id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   token_id VARCHAR(255),
   token LONG VARBINARY,
   authentication LONG VARBINARY
@@ -42,11 +46,14 @@ create table oauth_refresh_token (
  
 drop table if exists oauth_code;
 create table oauth_code (
-  code VARCHAR(255), authentication LONG VARBINARY
+  id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  code VARCHAR(255),
+  authentication LONG VARBINARY
 );
  
 drop table if exists oauth_approvals;
 create table oauth_approvals (
+    id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userId VARCHAR(255),
     clientId VARCHAR(255),
     scope VARCHAR(255),
@@ -57,7 +64,8 @@ create table oauth_approvals (
  
 drop table if exists ClientDetails;
 create table ClientDetails (
-  appId VARCHAR(255) PRIMARY KEY,
+  id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  appId VARCHAR(255),
   resourceIds VARCHAR(255),
   appSecret VARCHAR(255),
   scope VARCHAR(255),
