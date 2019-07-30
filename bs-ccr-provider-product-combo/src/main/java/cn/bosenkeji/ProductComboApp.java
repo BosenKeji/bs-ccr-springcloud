b8d16968a498415c6af1a8181dce571ad0c9a169
xivinChen <12250402127@qq.com>
2019-07-30 19:32:31 +0800

    规范命名

--- bs-ccr-provider-product-combo/src/main/java/cn/bosenkeji/ProductComboApp.java
+++ bs-ccr-provider-product-combo/src/main/java/cn/bosenkeji/ProductComboApp.java
@@ -14,10 +14,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @MapperScan("cn.bosenkeji.mapper")
-public class App 
+public class ProductComboApp
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(ProductComboApp.class,args);
     }
 }
