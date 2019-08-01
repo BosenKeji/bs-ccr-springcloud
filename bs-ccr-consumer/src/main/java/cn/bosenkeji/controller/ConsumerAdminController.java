0d22e7f0766d61d130fc894c2a7e2346d9599e60
hjh <hjhopq@163.com>
2019-08-01 10:40:38 +0800

    user-boss模块添加swagger和volidate

--- bs-ccr-consumer/src/main/java/cn/bosenkeji/controller/ConsumerAdminController.java
+++ bs-ccr-consumer/src/main/java/cn/bosenkeji/controller/ConsumerAdminController.java
@@ -4,9 +4,15 @@ import cn.bosenkeji.service.IAdminClientService;
 import cn.bosenkeji.util.Result;
 import cn.bosenkeji.vo.Admin;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 /**
@@ -18,35 +24,42 @@ import java.util.Optional;
  **/
 @RestController
 @RequestMapping("/admin")
+@Validated
+@Api
 public class ConsumerAdminController {
     @Resource
     private IAdminClientService adminClientService;
 
-    @GetMapping("/")
+    @GetMapping(value="/")
+    @ApiOperation(value = "获取管理员列表",httpMethod = "GET",nickname = "getAdminByPage")
     public PageInfo listByPage(
-            @RequestParam("pageNum") Integer pageNum,
-            @RequestParam("pageSize") Integer pageSize
-            ) {
+            @RequestParam(value = "pageNum",required = false,defaultValue = "1") @Min(1) @ApiParam(value = "起始页",type = "Integer") Integer pageNum,
+            @RequestParam(value = "pageSize",required = false,defaultValue = "10") @Min(1) @ApiParam(value = "起始页",type = "Integer") Integer pageSize
+    ) {
         return adminClientService.listByPage(pageNum,pageSize);
     }
 
     @GetMapping("/{id}")
-    public Optional<Admin> get(@PathVariable("id") Integer id) {
+    @ApiOperation(value = "获取指定id的管理员",httpMethod = "GET",nickname = "getAdminById")
+    public Optional<Admin> get(@PathVariable("id") @Min(1) @ApiParam(value = "管理员的id",type = "Integer",example = "1") Integer id) {
         return adminClientService.get(id);
     }
 
-    @PostMapping("/")
-    public Result add(Admin admin) {
+    @PostMapping(value = "/")
+    @ApiOperation(value = "添加管理员信息",httpMethod = "POST",nickname = "addAdmin")
+    public Result add (@NotNull @ApiParam(value = "管理员信息",required = true,type = "Admin") Admin admin) {
         return adminClientService.add(admin);
     }
 
-    @PutMapping("/")
-    public Result update(Admin admin) {
+    @PutMapping(value = "/")
+    @ApiOperation(value = "更新指定id的管理员",httpMethod = "PUT",nickname = "updateAdminById")
+    public Result update(@NotNull @ApiParam(value = "管理员信息",required = true,type = "Admin") Admin admin) {
         return adminClientService.update(admin);
     }
 
     @DeleteMapping("/{id}")
-    public Result delete(@PathVariable("id") Integer id) {
+    @ApiOperation(value = "删除指定id的管理员",httpMethod = "DELETE",nickname = "deleteAdminById")
+    public Result delete(@PathVariable("id") @Min(1) @ApiParam(value = "管理员的id",type = "Integer",example = "1") Integer id) {
         return adminClientService.delete(id);
     }
 }
--- bs-ccr-provider-user-boss/src/main/java/cn/bosenkeji/controller/AdminController.java
+++ bs-ccr-provider-user-boss/src/main/java/cn/bosenkeji/controller/AdminController.java
@@ -8,11 +8,16 @@ import cn.bosenkeji.service.AdminService;
 import cn.bosenkeji.util.Result;
 import cn.bosenkeji.vo.Admin;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +31,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/admin")
 @Validated
+@Api(tags = "Admin 管理员相关接口",value = "提供管理员相关的 Rest API接口")
 public class AdminController {
 
     @Resource
@@ -33,33 +39,38 @@ public class AdminController {
 
 
     @GetMapping(value="/")
+    @ApiOperation(value = "获取管理员列表",httpMethod = "GET",nickname = "getAdminByPage")
     public PageInfo listByPage(
-            @RequestParam("pageNum") Integer pageNum,
-            @RequestParam("pageSize") Integer pageSize
+            @RequestParam(value = "pageNum",required = false,defaultValue = "1") @Min(1) @ApiParam(value = "起始页",type = "Integer") Integer pageNum,
+            @RequestParam(value = "pageSize",required = false,defaultValue = "10") @Min(1) @ApiParam(value = "起始页",type = "Integer") Integer pageSize
     ) {
         return adminService.listByPage(pageNum,pageSize);
     }
 
     @PostMapping(value = "/")
-    public Result add (@RequestBody Admin admin) {
+    @ApiOperation(value = "添加管理员信息",httpMethod = "POST",nickname = "addAdmin")
+    public Result add (@RequestBody @NotNull @ApiParam(value = "管理员信息",required = true,type = "Admin") Admin admin) {
         return new Result(adminService.add(admin).filter((v)->v>=0)
                 .orElseThrow(()->new AddException(AdminEnum.ADMIN_EXIST)));
     }
 
     @DeleteMapping("/{id}")
-    public Result delete(@PathVariable("id") Integer id) {
+    @ApiOperation(value = "删除指定id的管理员",httpMethod = "DELETE",nickname = "deleteAdminById")
+    public Result delete(@PathVariable("id") @Min(1) @ApiParam(value = "管理员的id",type = "Integer",example = "1") Integer id) {
         return new Result(adminService.delete(id).filter((v)->v>=0)
                 .orElseThrow(()->new DeleteException(AdminEnum.ADMIN_NOT_FOUND)));
     }
 
     @PutMapping(value = "/")
-    public Result update(@RequestBody Admin admin) {
+    @ApiOperation(value = "更新指定id的管理员",httpMethod = "PUT",nickname = "updateAdminById")
+    public Result update(@RequestBody @NotNull @ApiParam(value = "管理员信息",required = true,type = "Admin") Admin admin) {
         return new Result(adminService.update(admin).filter((v)->v>=0)
                 .orElseThrow(()->new UpdateException(AdminEnum.ADMIN_NOT_FOUND)));
     }
 
     @GetMapping("/{id}")
-    public Optional<Admin> get(@PathVariable Integer id) {
+    @ApiOperation(value = "获取指定id的管理员",httpMethod = "GET",nickname = "getAdminById")
+    public Optional<Admin> get(@PathVariable("id") @Min(1) @ApiParam(value = "管理员的id",type = "Integer",example = "1") Integer id) {
         return adminService.get(id);
     }
 
