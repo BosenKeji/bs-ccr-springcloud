bfb3a1fbeb3ff5d56d890f39a6d6954be5bc50d7
yuxuewen <8586826@qq.com>
2019-08-22 00:30:10 +0800

    aws set

--- bs-ccr-aws-deal/src/main/java/cn/bosenkeji/DealApp.java
+++ bs-ccr-aws-deal/src/main/java/cn/bosenkeji/DealApp.java
@@ -3,8 +3,10 @@ package cn.bosenkeji;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DealApp {
 
     public static void main(String[] args) {
---
+++ bs-ccr-aws-deal/src/main/java/cn/bosenkeji/controller/TestController.java
@@ -0,0 +1,42 @@
+/**
+ * FileName: TestController
+ * Author: xivin
+ * Date: 2019-08-20 17:50
+ * Description:
+ */
+package cn.bosenkeji.controller;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Cacheable(value = "test",key = "#id")
+    @GetMapping("/test")
+    public String test(int id) {
+        return "test"+id;
+    }
+
+
+    @GetMapping("/testTemplate")
+    public String testTemplate(int id) {
+        try{
+            redisTemplate.opsForValue().set("template"+id,"test template!!!");
+            return "success"+redisTemplate.opsForValue().get("template"+id);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return "fail"+e.getLocalizedMessage();
+        }
+
+    }
+
+
+}
