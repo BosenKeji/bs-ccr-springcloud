3626dba99a4657e8d7a926369bac70ee6534d383
xivin <1250402127@qq.com>
2019-08-21 18:59:55 +0800

    aws cache

--- bs-ccr-aws-redis/src/main/java/cn/bosenkeji/controller/TestController.java
+++ bs-ccr-aws-redis/src/main/java/cn/bosenkeji/controller/TestController.java
@@ -7,15 +7,36 @@
 package cn.bosenkeji.controller;
 
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class TestController {
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Cacheable(value = "test",key = "#id")
     @GetMapping("/test")
     public String test(int id) {
         return "test"+id;
     }
+
+
+    @GetMapping("/testTemplate")
+    public String testTemplate(int id) {
+        try{
+            redisTemplate.opsForValue().set("template","test template!!!");
+            return "success"+redisTemplate.opsForValue().get("template");
+        }catch (Exception e) {
+            e.printStackTrace();
+            return "fail"+e.getLocalizedMessage();
+        }
+
+    }
+
+
 }
