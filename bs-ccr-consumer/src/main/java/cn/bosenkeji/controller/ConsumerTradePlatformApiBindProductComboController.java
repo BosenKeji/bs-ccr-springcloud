90092c7f9fec15219b62a444adbc3a87199bd0fd
xivin <1250402127@qq.com>
2019-08-28 17:57:23 +0800

    添加用户只能操作自己的数据权限

--- bs-ccr-consumer/src/main/java/cn/bosenkeji/controller/ConsumerTradePlatformApiBindProductComboController.java
+++ bs-ccr-consumer/src/main/java/cn/bosenkeji/controller/ConsumerTradePlatformApiBindProductComboController.java
@@ -1,10 +1,12 @@
 package cn.bosenkeji.controller;
 
 import cn.bosenkeji.service.ITradePlatformApiBindProductComboClientService;
+import cn.bosenkeji.service.impl.CustomUserDetailsImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -31,7 +33,8 @@ public class ConsumerTradePlatformApiBindProductComboController {
                                   @RequestParam(value="pageSize",defaultValue="10") int pageSize,
                                   @RequestParam("userId") @ApiParam(value = "用户ID",required = true,type = "integer",example = "1") int userId) {
 
-       return iTradePlatformApiBindProductComboClientService.getListByUserId(pageNum,pageSize,userId);
+        int currentUserId=this.getCurrentUser().getId();
+       return iTradePlatformApiBindProductComboClientService.getListByUserId(pageNum,pageSize,currentUserId);
 
 
     }
@@ -43,40 +46,30 @@ public class ConsumerTradePlatformApiBindProductComboController {
                                                           @RequestParam(value = "pageSize",defaultValue = "10") int pageSize,
                                                           @RequestParam("userId") @ApiParam(value = "用户ID",required = true,type = "integer",example = "1") int userId) {
 
-        return iTradePlatformApiBindProductComboClientService.getNoBindTradePlatformApiListByUserId(pageNum,pageSize,userId);
+        int currentUserId=this.getCurrentUser().getId();
+        return iTradePlatformApiBindProductComboClientService.getNoBindTradePlatformApiListByUserId(pageNum,pageSize,currentUserId);
     }
 
-    /*@GetMapping("/get_no_bind_user_product_combo_list_by_user_id")
-    @ApiOperation(value = "根据用户ID 获取用户未绑定的用户套餐列表 api接口"
-            ,httpMethod = "GET",nickname = "getNoBindUserProductComboListByUserIdWithPage")
-    public Object getNoBindUserProductComboListByUserId(@RequestParam( value="pageNum",defaultValue="1") int pageNum,
-                                                          @RequestParam(value = "pageSize",defaultValue = "10") int pageSize,
-                                                          @RequestParam("userId") @ApiParam(value = "用户ID",required = true,type = "integer",example = "1") int userId) {
-
-        return iTradePlatformApiBindProductComboClientService.getNoBindUserProductComboListByUserId(pageNum,pageSize,userId);
-    }*/
-
-   /* @PostMapping("/")
-    @ApiOperation(value = "添加 交易平台api绑定用户套餐 api接口"
-            ,httpMethod = "POST",nickname = "addTradePlatformApiBindProductComboList")
-    public Object add(@RequestBody @ApiParam(value = "交易谱平台api绑定用户套餐实体",required = true,type = "string") TradePlatformApiBindProductCombo tradePlatformApiBindProductCombo) {
-
-      return iTradePlatformApiBindProductComboClientService.addTradePlatformApiBindProductCombo(tradePlatformApiBindProductCombo);
-
-    }*/
 
     @PutMapping("/{id}")
     @ApiOperation(value = "更新 交易平添api绑定用户套餐 api接口",httpMethod = "PUT",nickname = "updateTradePlatformApiBindProductCombo")
     public Object updateTradePlatformApiBindProductCombo(@PathVariable("id") @ApiParam(value = "交易平台绑定用户套餐ID",required = true,type = "integer",example = "1") int id,
                                                          @RequestParam("tradePlatformApiId") @ApiParam(value = "交易平台api ID",required = true,type = "integer",example = "1") int tradePlatformApiId,
                                                          @RequestParam("userId") @ApiParam(value = "用户ID",required = true,type = "integer",example = "1") int userId) {
-        return iTradePlatformApiBindProductComboClientService.updateTradePlatformApiBindProductCombo(id,tradePlatformApiId,userId);
+        int currentUserId=this.getCurrentUser().getId();
+        return iTradePlatformApiBindProductComboClientService.updateTradePlatformApiBindProductCombo(id,tradePlatformApiId,currentUserId);
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation(value="解除 交易品台绑定用户套餐",httpMethod = "DELETE",nickname = "deleteTradePlatformApiBindProductCombo")
     public Object deleteOneApiBindCombo(@PathVariable("id") @ApiParam(value = "交易平台绑定用户套餐ID",required = true,type = "integer",example = "1") int id,
                          @RequestParam("userId") @ApiParam(value = "用户ID",required = true,type = "integer",example = "1") int userId) {
-        return iTradePlatformApiBindProductComboClientService.deleteTradePlatformApiBindProductCombo(id,userId);
+        int currentUserId=this.getCurrentUser().getId();
+        return iTradePlatformApiBindProductComboClientService.deleteTradePlatformApiBindProductCombo(id,currentUserId);
+    }
+
+    public CustomUserDetailsImpl getCurrentUser() {
+        CustomUserDetailsImpl principal = (CustomUserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal;
     }
 }
