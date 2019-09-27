318f09035927c4dac154ae53903546f9b6b619c7
xivin <1250402127@qq.com>
2019-09-27 17:03:01 +0800

    alter flway

---
+++ bs-ccr-flyway/src/main/resources/db/migration/V1.2.5__Alter_combo_day_by_admin.sql
@@ -0,0 +1,3 @@
+alter table user_product_combo_day_by_admin add `order_number` varchar(128) NOT NULL;
+alter table user_product_combo_day_by_admin add  `remark` varchar(80) NOT NULL;
+alter table `combo_day_reason_id` int(11) NOT NULL;
