b63c55c5686eb2c0e6c9dd27843266165dab4da6
xivin <1250402127@qq.com>
2019-09-27 17:25:45 +0800

    alter flway

--- bs-ccr-flyway/src/main/resources/db/migration/V1.2.5__Alter_combo_day_by_admin.sql
+++ bs-ccr-flyway/src/main/resources/db/migration/V1.2.5__Alter_combo_day_by_admin.sql
@@ -1,3 +1,25 @@
-alter table user_product_combo_day_by_admin add `order_number` varchar(128) NOT NULL;
-alter table user_product_combo_day_by_admin add  `remark` varchar(80) NOT NULL;
-alter table `combo_day_reason_id` int(11) NOT NULL;
+DROP PROCEDURE IF EXISTS schema_change;
+DELIMITER //
+CREATE PROCEDURE schema_change()
+BEGIN
+    DECLARE  CurrentDatabase VARCHAR(100);
+    SELECT DATABASE() INTO CurrentDatabase;
+
+    IF NOT	EXISTS(SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = 'user_product_combo_day_by_admin' AND COLUMN_NAME = 'order_number')
+    then
+				alter table user_product_combo_day_by_admin add `order_number` varchar(128) NOT NULL DEFAULT '';
+    END IF;
+
+    IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = 'user_product_combo_day_by_admin' AND COLUMN_NAME = 'remark')
+    then
+        alter table user_product_combo_day_by_admin add  `remark` varchar(80) NOT NULL DEFAULT '';
+    END IF;
+
+    IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = CurrentDatabase AND TABLE_NAME = 'user_product_combo_day_by_admin' AND COLUMN_NAME = 'combo_day_reason_id')
+        then
+            alter table user_product_combo_day_by_admin add `combo_day_reason_id` int(11) NOT NULL DEFAULT 1;
+    END IF;
+end //
+DELIMITER ;
+CALL schema_change();
+DROP PROCEDURE IF EXISTS schema_change;
