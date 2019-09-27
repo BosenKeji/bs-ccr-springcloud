4f3b29efc02947590d031172de34561900e3c9d3
admin <1206303099@qq.com>
2019-09-27 16:19:19 +0800

    add RSA

--- bs-ccr-service/src/main/java/cn/bosenkeji/service/ITradePlatformApiClientService.java
+++ bs-ccr-service/src/main/java/cn/bosenkeji/service/ITradePlatformApiClientService.java
@@ -41,10 +41,4 @@ public interface ITradePlatformApiClientService {
 
     @GetMapping("/trade_platform_apis/all")
     List<TradePlatformApi> findAll();
-
-    @GetMapping("/trade_platform_apis/public_key")
-    String getPubKey();
-
-    @GetMapping("/trade_platform_apis/public_key")
-    String getPriKey();
 }
