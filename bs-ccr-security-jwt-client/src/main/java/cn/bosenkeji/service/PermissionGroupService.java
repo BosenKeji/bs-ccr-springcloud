873605753f49bf3aa6e33caa6dcf3dd7817b8d5f
hjh <hjhopq@163.com>
2019-10-08 16:42:34 +0800

    添加权限模块管理

---
+++ bs-ccr-security-jwt-client/src/main/java/cn/bosenkeji/service/PermissionGroupService.java
@@ -0,0 +1,28 @@
+package cn.bosenkeji.service;
+
+import cn.bosenkeji.vo.permission.Permission;
+import cn.bosenkeji.vo.permission.PermissionGroup;
+import cn.bosenkeji.vo.permission.PermissionGroupOther;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PermissionGroupService {
+
+    Optional<Integer> savePermissionGroup(PermissionGroup permissionGroup);
+
+    List<PermissionGroup> listPermissionGroup();
+
+    PermissionGroup getPermissionGroupById(Integer id);
+
+    Optional<Integer> updatePermissionGroupById(PermissionGroup permissionGroup);
+
+    Optional<Integer> deletePermissionGroupById(Integer id);
+
+    PermissionGroupOther getPermissionGroupJoin(Integer id);
+
+    List<PermissionGroupOther> listPermissionGroupJoin();
+
+    Optional<Integer> savePermissionGroupJoin(Integer groupId, List<Permission> permissionList);
+
+}
