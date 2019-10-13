edf008a6687238f04756e34dc06eddf7c0d4fd78
yuxuewen <8586826@qq.com>
2019-10-14 00:05:18 +0800

    Revert "添加权限模块管理"
    This reverts commit 873605753f49bf3aa6e33caa6dcf3dd7817b8d5f.

--- bs-ccr-security-jwt-client/src/main/java/cn/bosenkeji/JwtClientApp.java
+++ bs-ccr-security-jwt-client/src/main/java/cn/bosenkeji/JwtClientApp.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @ClassName App
@@ -15,7 +14,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 **/
 @SpringBootApplication
 @MapperScan("cn.bosenkeji.mapper")
-@EnableTransactionManagement
 public class JwtClientApp {
     public static void main(String[] args) {
         SpringApplication.run(JwtClientApp.class, args);
