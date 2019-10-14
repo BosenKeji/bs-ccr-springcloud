19aa8acb2eea80a934db5758014afbb5c387028c
hjh <hjhopq@163.com>
2019-10-14 19:20:52 +0800

    修改策略浮点数问题

---
+++ bs-ccr-flyway/src/main/resources/db/migration/V1.2.12__Alter_strategy_table.sql
@@ -0,0 +1,3 @@
+alter table strategy_attribute modify column stop_profit_ratio int not null;
+alter table strategy_attribute modify column stop_profit_trace_trigger_rate int not null;
+alter table strategy_attribute modify column stop_profit_trace_drop_rate int not null;
