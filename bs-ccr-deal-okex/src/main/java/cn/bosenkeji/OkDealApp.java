c93bf3e787f04c3bf2b03602feada3c1926869c3
yuxuewen <8586826@qq.com>
2019-12-01 02:47:38 +0800

    deal @EnableDiscoveryClient

--- bs-ccr-deal-okex/src/main/java/cn/bosenkeji/OkDealApp.java
+++ bs-ccr-deal-okex/src/main/java/cn/bosenkeji/OkDealApp.java
@@ -5,10 +5,12 @@ import cn.bosenkeji.messaging.MySink;
 import cn.bosenkeji.messaging.MySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableBinding({MySource.class, MySink.class})
+@EnableDiscoveryClient
 public class OkDealApp {
 
     public static void main(String[] args) {
--- bs-ccr-deal/src/main/java/cn/bosenkeji/DealApp.java
+++ bs-ccr-deal/src/main/java/cn/bosenkeji/DealApp.java
@@ -6,12 +6,13 @@ import cn.bosenkeji.messaging.MySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableBinding({MySource.class, MySink.class})
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class DealApp {
 
     public static void main(String[] args) {
