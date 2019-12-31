f698e1c787fa57f00cb28b9565c8f8c1a3629653
lwj <1206303099@qq.com>
2019-12-31 16:41:39 +0800

    修复了一些bug

--- bs-ccr-order/src/main/java/cn/bosenkeji/OrderApp.java
+++ bs-ccr-order/src/main/java/cn/bosenkeji/OrderApp.java
@@ -1,8 +1,9 @@
 package cn.bosenkeji;
 
-import cn.bosenkeji.sink.OrderSink;
+import cn.bosenkeji.messaging.OrderSink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -11,8 +12,8 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
  * @author CAJR
  * @date 2019/12/26 7:24 下午
  */
-//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication
 @EnableBinding({OrderSink.class})
 @EnableDiscoveryClient
 @EnableFeignClients("cn.bosenkeji.service")
