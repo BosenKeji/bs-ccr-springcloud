ea3784e4f8dc3aa73f137ba89010dc098d771c98
lwj <1206303099@qq.com>
2019-12-31 16:10:46 +0800

    修复了一些bug

--- bs-ccr-order/src/main/java/cn/bosenkeji/OrderApp.java
+++ bs-ccr-order/src/main/java/cn/bosenkeji/OrderApp.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
  */
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
-//@EnableBinding({OrderSink.class})
+@EnableBinding({OrderSink.class})
 @EnableDiscoveryClient
 @EnableFeignClients("cn.bosenkeji.service")
 public class OrderApp {
